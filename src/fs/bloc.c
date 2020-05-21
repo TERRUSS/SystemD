dd5f6e5a67c1c7f1e0ce8dfb36d2cfaadbcf6c1e
Poulpy <paul.repain@yahoo.com>
2020-05-21 09:37:51 +0200

    Move bloc functions to bloc.c (2)

---
+++ src/fs/bloc.c
@@ -0,0 +1,60 @@
+#include "fs/bloc.h"
+
+/**
+ * Adds a bloc id to an inode
+ * TODO add return code
+ * on success : returns 1
+ * on failure : returns 0
+ */
+int add_bloc(struct inode *i, struct bloc *b) {
+	if (i->bloc_count == BLOC_IDS_COUNT) {
+		perror("Can't add anymore blocs to the inode !");
+		return 0;
+	} else {
+		i->bloc_ids[i->bloc_count] = b->id;
+		i->bloc_count++;
+		return 1;
+	}
+}
+
+/**
+ * Returns a bloc
+ *
+ * filename must not be NULL (except for root)
+ * content can be NULL
+ */
+struct bloc new_bloc(const char *filename, const char *content) {
+	struct bloc b;
+
+	b.id = rand();
+
+	// TODO strncpy for safer copy
+	strcpy(b.filename, filename);
+
+	if (content == NULL) {
+		strcpy(b.content, "");
+	} else {
+		strcpy(b.content, content);
+	}
+
+	return b;
+}
+
+/**
+ * Deletes a bloc : set the id to DELETED (0)
+ */
+void delete_bloc(struct bloc *b) {
+	b->id = DELETED;
+}
+
+/**
+ * Prints a bloc to the terminal
+ */
+void print_bloc(struct bloc *b) {
+	printf("<BLOC> id:%d", b->id);
+	printf(" filename:%s", b->filename);
+	printf(" content:%s", b->content);
+	puts("");
+}
+
+
---
+++ src/fs/bloc.h
@@ -0,0 +1,25 @@
+#ifndef BLOC_H
+#define BLOC_H
+
+#include "fs/inode.h"
+
+#define BLOC_SIZE (512)
+#define FILENAME_COUNT (15)
+
+
+struct bloc {
+	/* const */
+	unsigned int id;
+
+	char filename[FILENAME_COUNT];
+
+	char content[BLOC_SIZE];
+};
+
+int add_bloc(struct inode *i, struct bloc *b);
+struct bloc new_bloc(const char *filename, const char *content);
+void delete_bloc(struct bloc *b);
+void print_bloc(struct bloc *b);
+
+#endif
+
