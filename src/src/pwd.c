29cef127185c9f9a0119c4f09965e210d89238d4
TERRUSS <olivier.benaben@protonmail.com>
2020-05-29 23:16:35 +0200

    pwd ✓

--- src/src/pwd.c
+++ src/src/pwd.c
@@ -5,9 +5,29 @@
 int main(int argc, char const *argv[]) {
 	unsigned int cur_dir = get_pwd_id();
 
-	char * fn = get_dirname_by_id(cur_dir);
-	printf("%s\n", fn);
-	free(fn);
+
+	struct inode cur = get_inode_by_id(cur_dir);
+	struct inode inodelist[100];
+	int count = 0, *subid;
+	struct bloc b;
+
+	inodelist[0] = cur;
+
+	while( inodelist[count].id != ROOT_ID ){
+		count++;
+		b = get_bloc_by_id(inodelist[count-1].bloc_ids[0]);
+		strsplt(b.content, &subid, ',');
+		
+		inodelist[count] = get_inode_by_id(subid[1]);
+		printf("%d : %d\n", count, inodelist[count].id);	
+	}
+
+	for (int i = count; i > -1; i--){
+		printf("%s", get_dirname_by_id(inodelist[i].id));
+	}
+	
+	
+	printf("\n");
 
 	return 0;
 }
